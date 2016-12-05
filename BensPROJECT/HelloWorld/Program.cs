97024af4a44eccc2df0a7fcbeb4a4e5ba94591f3
benjamin.bowes <benjamin.bowes@a1baab40-a34d-48c9-b80e-3ada5a7d259e>
2016-12-05 11:00:24 +0000

    First Version of HelloWorld

---
+++ BensPROJECT/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld    //
+{
+    class Program   //CLASS DECLARATION
+    {
+        static void Main(string[] args)     // METHOD
+        {
+            Console.WriteLine("Hello World!");      // TASK
+
+            string userInput = Console.ReadLine();     //READ TASK IN CONSOLE WINDOW - USER INPUT
+
+            Console.WriteLine(userInput);    // Write out var
+
+            Console.ReadLine();         //READ TASK
+
+        }
+    }
+}
