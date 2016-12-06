b4add8f6d0df306eb178dc70ad02a08c5f91c93e
benjamin.bowes <benjamin.bowes@a1baab40-a34d-48c9-b80e-3ada5a7d259e>
2016-12-06 12:35:56 +0000

    FourPillars GUIDE

---
+++ FourPillars/FourPillars.Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Encapsulation // ENCAPSULATION - GROUPING TOGETHER OF DATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ FourPillars/FourPillars.Inheritance/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourPillars.Inheritance
+{
+   public abstract class User
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+
+    }
+    public class Buyer : User
+    {
+
+    }
+    
+    public class Seller : User
+    {
+
+    }
+
+    public class Admin : User
+    {
+        public string password { get; set; }
+
+    }
+}
