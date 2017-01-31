f6a8b2b0518d640bdac6a30903798ac3dec90f7e
benjamin.bowes <benjamin.bowes@a1baab40-a34d-48c9-b80e-3ada5a7d259e>
2017-01-31 16:25:52 +0000

    added a line in register, still need to defend

--- GROUP PROJECT/SocialNetwork/SocialNetwork.Logic/IUserAccountLogic.cs
+++ GROUP PROJECT/SocialNetwork/SocialNetwork.Logic/IUserAccountLogic.cs
@@ -9,7 +9,7 @@ namespace SocialNetwork.Logic
 {
     public interface IUserAccountLogic
     {
-        void Login(string username, string password);
+        bool Login(string username, string password);
         bool LoginDetailVerification(string username, string password);
         void Logout(int id);
         void Register(User userToAdd);
@@ -18,69 +18,5 @@ namespace SocialNetwork.Logic
         void UpdateInfo(int id, string username, string password);
     }
 
-    public class UserAccountLogic : IUserAccountLogic
-    {
-        Repository<User> _userRepository;
-
-        public UserAccountLogic(Repository<User> userRepository)
-        {
-            _userRepository = userRepository;
-        }
-
-        public void Login(string username, string password)
-        {
-
-            //if(username.Count<char>() > 255) 
-            //{
-            //    throw new InputExceedsSpecifiedLimitException();
-            //}
-            //if (password.Count<char>() > 255) 
-            //{
-            //    throw new InputExceedsSpecifiedLimitException();
-            //}
-        }
-
-        public bool LoginDetailVerification(string username, string password)
-        {
-            User currentUser = _userRepository.First(u => u.username == username);
-
-            if (currentUser.username == username && currentUser.password == password)
-            {
-                return true;
-            } return false;
-          
-        }
-
-        public void Logout(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Register(User userToAdd)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ViewAccountInfo(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddFriend(int userId, int friendId)
-        {
-            //probably wrong
-
-            User currentUser = _userRepository.First(u => u.userId == userId);
-
-            User userToAdd = _userRepository.First(u => u.userId == userId);
-
-            currentUser.friends.ToList().Add(userToAdd);
-            
-        }
-
-        public void UpdateInfo(int id, string username, string password)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
 }
