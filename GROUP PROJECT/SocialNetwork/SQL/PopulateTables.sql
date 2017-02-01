a5b524893374b167e5edd6b5b50617d32271b16e
benjamin.bowes <benjamin.bowes@a1baab40-a34d-48c9-b80e-3ada5a7d259e>
2017-02-01 12:55:08 +0000

    Made us all friends :)

--- GROUP PROJECT/SocialNetwork/SQL/PopulateTables.sql
+++ GROUP PROJECT/SocialNetwork/SQL/PopulateTables.sql
@@ -4,6 +4,8 @@
  -- DROP TABLE Posts;
  -- DROP TABLE Users;
  -- DROP TABLE Groups;
+ -- DROP TABLE UserFriends
+ -- DROP TABLE UserGroups
  
  
  --	DELETE ENTRIES FROM DATABASE (Not Tables)
@@ -11,12 +13,14 @@ DELETE FROM Comments
 DELETE FROM Posts
 DELETE FROM Groups
 DELETE FROM Users
+DELETE FROM UserFriends
+DELETE FROM UserGroups
 
  -- RESEED PRIMARY KEY VALUES (START FROM 0 AGAIN)
- DBCC CHECKIDENT (Users, RESEED, 0)
- DBCC CHECKIDENT (Comments, RESEED, 0)
- DBCC CHECKIDENT (Posts, RESEED, 0)
- DBCC CHECKIDENT (Groups, RESEED, 0)
+DBCC CHECKIDENT (Users, RESEED, 0)
+DBCC CHECKIDENT (Comments, RESEED, 0)
+DBCC CHECKIDENT (Posts, RESEED, 0)
+DBCC CHECKIDENT (Groups, RESEED, 0)
 
 
  -- Populate Users
@@ -44,9 +48,48 @@ INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 4);
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 5);
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 6);
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 7);
+
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 1);
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 6);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 7);
+
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 2);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 6);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (3, 7);
+
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 2);
 INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 6);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 7);
+
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 2);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 6);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (5, 7);
+
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 2);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (6, 7);
+
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 2);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (7, 6);
 
 SELECT U1.username, U2.username FROM UserFriends
 INNER JOIN Users U1 ON U1.userId = UserRefId
