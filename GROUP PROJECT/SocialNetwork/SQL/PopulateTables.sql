1c1ed167e25c476dd5931b60298b48fcd534e426
benjamin.bowes <benjamin.bowes@a1baab40-a34d-48c9-b80e-3ada5a7d259e>
2017-02-01 10:45:30 +0000

    Added many to many table inserts

--- GROUP PROJECT/SocialNetwork/SQL/PopulateTables.sql
+++ GROUP PROJECT/SocialNetwork/SQL/PopulateTables.sql
@@ -7,10 +7,10 @@
  
  
  --	DELETE ENTRIES FROM DATABASE (Not Tables)
- -- DELETE FROM Comments
- -- DELETE FROM Posts
- -- DELETE FROM Users
- -- DELETE FROM Groups
+DELETE FROM Comments
+DELETE FROM Posts
+DELETE FROM Groups
+DELETE FROM Users
 
  -- RESEED PRIMARY KEY VALUES (START FROM 0 AGAIN)
  DBCC CHECKIDENT (Users, RESEED, 0)
@@ -35,14 +35,51 @@ VALUES ('dmason', 'password', 'Daniel Mason', 'Male');
 INSERT INTO Users(username, password, fullName, gender)
 VALUES ('bbowes', 'password', 'Benjamin Bowes', 'Male');
 
+SELECT userId, username, fullName, gender FROM Users
+
+ -- Populate User-Friends
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 2);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 4);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 5);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 6);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (1, 7);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 1);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (2, 3);
+INSERT INTO UserFriends(UserRefId, FriendRefId) VALUES (4, 3);
+
+SELECT U1.username, U2.username FROM UserFriends
+INNER JOIN Users U1 ON U1.userId = UserRefId
+INNER JOIN Users U2 ON U2.userId = FriendRefId;
+
+ -- Populate Groups
+INSERT INTO Groups(groupName, owner_userId) VALUES ('Cerf Squad', 1);
+
+SELECT groupID, groupName, owner_userId FROM Groups;
+
+ -- Populate User-Groups
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 1);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 2);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 3);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 4);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 5);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 6);
+INSERT INTO UserGroups(GroupRefId, UserRefId) VALUES (1, 7);
+
+SELECT G.groupName, U.username FROM UserGroups
+INNER JOIN Users U ON U.userId = UserRefId
+INNER JOIN Groups G ON G.groupId = GroupRefId;
+
  -- Populate Posts
 INSERT INTO Posts(time, likes, title, user_userId, Discriminator) VALUES (GETDATE(), 10, 'I Love C#', 1, 'UserPost');
-SELECT postId, time, likes, title, language, content, Discriminator FROM Posts;
+INSERT INTO Posts(time, likes, title, group_groupId, Discriminator) VALUES (GETDATE(), 20, 'I Despise C#', 1, 'GroupPost');
+
+SELECT postId, time, likes, title, language, content, Discriminator, group_groupID, user_userId FROM Posts;
+
+
 
  -- Populate Comments
 INSERT INTO Comments(content, post_postId, user_userId) VALUES ('That was so funny haha lol - Bish', 1, 2);
-SELECT commentId, content, post_postId, user_userId FROM Comments;
+INSERT INTO Comments(content, post_postId, user_userId) VALUES ('That wasnt funny :(', 2, 1);
 
- -- Populate Groups
- INSERT INTO Groups(groupName, owner_userId) VALUES ('Cerf Squad', 1);
- SELECT groupID, groupName, owner_userId FROM Groups;
+SELECT commentId, content, post_postId, user_userId FROM Comments;
